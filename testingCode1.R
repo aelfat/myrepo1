f451286bf06329756233a65c7613139ec4f31c7f
aelfat <aelfat989@gmail.com>
2022-01-20 22:55:42 -0600

    new script

---
+++ testingCode1.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+
+
+# this is a game of addition
+# choose the an accepted operation, +
+testQuestion <- readline("choose an operation: +, -, /, * ")
+
+
+  if(testQuestion== "+"){
+        x1=sample(-100:300,1)
+        x2=sample(100:600,1)
+        
+        answer1 <- readline(paste0(x1, " + ", x2," = "))
+        
+        ifelse(answer1==x1+x2,print("Correct!"),print("Inorrect!"))
+        
+    
+  }else{
+        print("we only do addition!")
+  }
+
